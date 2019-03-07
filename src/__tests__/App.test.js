dcc1605173c2fcc46d822d34b7b819b5c0ad294d
aknishi <adriankawanishi@gmail.com>
2019-03-06 23:38:51 -0800

    Add test for checking winner in rows, cols, diags, none

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -18,15 +18,15 @@ describe('<App>', () => {
     const emptyGrid = ['', '', '', '', '', '', '', '', ''];
 
     it('should be initialized with grid of nine empty spaces', () => {
-      expect(myApp.state.grid).toEqual(emptyGrid)
+      expect(myApp.state.grid).toEqual(emptyGrid);
     });
 
     it('should keep track of the current player', () => {
-      expect(myApp.state.currentPlayer).toBeTruthy()
+      expect(myApp.state.currentPlayer).toBeTruthy();
     });
 
     it('should keep a history of moves', () => {
-      expect(myApp.state.history).toBeTruthy()
+      expect(myApp.state.history).toBeTruthy();
     });
   });
 
@@ -35,8 +35,8 @@ describe('<App>', () => {
   });
 
   it('should pass the grid to Board as props', () => {
-    const board = wrapper.find('Board')
-    expect(board.props().grid).toBeTruthy()
+    const board = wrapper.find('Board');
+    expect(board.props().grid).toBeTruthy();
   });
 
   it('renders prompt message', () => {
@@ -49,16 +49,16 @@ describe('<App>', () => {
     const initialGrid = ['X', 'O', 'X', '', '', '', '', '', '',];
 
     it('shows a reset button', () => {
-      const wrapper = shallow(<App />)
-      expect(wrapper.find('button').first().text()).toEqual('Reset')
+      const wrapper = shallow(<App />);
+      expect(wrapper.find('button').first().text()).toEqual('Reset');
     });
 
     it('reset grids when clicked', () => {
       const wrapper = mount(<App />);
-      wrapper.instance().setState({ grid: initialGrid })
-      wrapper.update()
-      wrapper.find("#reset-btn").simulate("click")
-      wrapper.update()
+      wrapper.instance().setState({ grid: initialGrid });
+      wrapper.update();
+      wrapper.find("#reset-btn").simulate("click");
+      wrapper.update();
       expect(wrapper.instance().state.grid).toEqual(emptyGrid);
     });
   })
@@ -76,9 +76,9 @@ describe('<App>', () => {
     it('undoes when clicked', () => {
       const wrapper = mount(<App />);
       wrapper.instance().setState({ grid: initialGrid, history: initialHistory })
-      wrapper.update()
-      wrapper.find("#undo-btn").simulate("click")
-      wrapper.update()
+      wrapper.update();
+      wrapper.find("#undo-btn").simulate("click");
+      wrapper.update();
       expect(wrapper.instance().state.grid).toEqual(resultGrid);
       expect(wrapper.instance().state.history).toEqual(resultHistory);
     });
@@ -87,8 +87,8 @@ describe('<App>', () => {
 
   describe('switchPlayers function', () => {
     it('should switch players when called', () => {
-      wrapper.instance().switchPlayers()
-      wrapper.update()
+      wrapper.instance().switchPlayers();
+      wrapper.update();
       expect(wrapper.instance().state.currentPlayer).toEqual('O')
     });
   });
@@ -96,18 +96,18 @@ describe('<App>', () => {
   describe('addMark function', () => {
     it('should update the grid when called', () => {
       const wrapper = shallow(<App />);
-      wrapper.instance().addMark(1)
-      wrapper.update()
+      wrapper.instance().addMark(1);
+      wrapper.update();
       expect(wrapper.instance().state.grid[1]).toEqual('X')
     });
 
     it('should call switch players after being called', () => {
       const wrapper = mount(<App />);
-      expect(wrapper.instance().state.currentPlayer).toEqual('X')
+      expect(wrapper.instance().state.currentPlayer).toEqual('X');
       const spy = jest.spyOn(wrapper.instance(), 'switchPlayers');
       wrapper.update();
-      wrapper.instance().addMark(1)
-      wrapper.update()
+      wrapper.instance().addMark(1);
+      wrapper.update();
       expect(spy).toHaveBeenCalled();
       expect(wrapper.instance().state.currentPlayer).toEqual('O')
     });
@@ -115,18 +115,18 @@ describe('<App>', () => {
     it('should not add a mark if square not empty', () => {
       const wrapper = shallow(<App />);
       jest.spyOn(window, 'alert').mockImplementation(() => { });
-      wrapper.instance().addMark(1)
-      wrapper.update()
-      wrapper.instance().addMark(1)
-      wrapper.update()
-      expect(wrapper.instance().state.grid[1]).toEqual('X')
+      wrapper.instance().addMark(1);
+      wrapper.update();
+      wrapper.instance().addMark(1);
+      wrapper.update();
+      expect(wrapper.instance().state.grid[1]).toEqual('X');
       expect(window.alert).toBeCalled();
     });
 
     it('should add to the move to the history after mark is added ', () => {
       const wrapper = shallow(<App />);
-      wrapper.instance().addMark(1)
-      wrapper.update()
+      wrapper.instance().addMark(1);
+      wrapper.update();
       expect(wrapper.instance().state.history[0]).toEqual(1)
     });
   })
@@ -138,27 +138,27 @@ describe('<App>', () => {
     it('should clear all marks from grid', () => {
       const wrapper = mount(<App />);
       wrapper.instance().setState({ grid: initialGrid })
-      wrapper.update()
-      wrapper.instance().reset()
-      wrapper.update()
+      wrapper.update();
+      wrapper.instance().reset();
+      wrapper.update();
       expect(wrapper.instance().state.grid).toEqual(emptyGrid);
     });
 
     it("it switches back to Player1's turn after reset ", () => {
       const wrapper = mount(<App />);
-      wrapper.instance().setState({ grid: initialGrid, currentPlayer: "O" })
-      wrapper.update()
-      wrapper.instance().reset()
-      wrapper.update()
+      wrapper.instance().setState({ grid: initialGrid, currentPlayer: "O" });
+      wrapper.update();
+      wrapper.instance().reset();
+      wrapper.update();
       expect(wrapper.instance().state.currentPlayer).toEqual('X');
     });
 
     it("it changes message to prompt Player1's turn ", () => {
       const wrapper = mount(<App />);
-      wrapper.instance().setState({ grid: initialGrid, currentPlayer: "O" })
-      wrapper.update()
-      wrapper.instance().reset()
-      wrapper.update()
+      wrapper.instance().setState({ grid: initialGrid, currentPlayer: "O" });
+      wrapper.update();
+      wrapper.instance().reset();
+      wrapper.update();
       expect(wrapper.instance().state.message).toEqual("Player 1's Turn");
     });
   })
@@ -171,10 +171,10 @@ describe('<App>', () => {
 
     it('should remove last move from grid and from history', () => {
       const wrapper = mount(<App />);
-      wrapper.instance().setState({ grid: initialGrid, history: initialHistory })
-      wrapper.update()
-      wrapper.instance().undo()
-      wrapper.update()
+      wrapper.instance().setState({ grid: initialGrid, history: initialHistory });
+      wrapper.update();
+      wrapper.instance().undo();
+      wrapper.update();
       expect(wrapper.instance().state.grid).toEqual(resultGrid);
       expect(wrapper.instance().state.history).toEqual(resultHistory);
     });
@@ -187,9 +187,9 @@ describe('<App>', () => {
         currentPlayer: "O",
         message: "Player 2's Turn"
       })
-      wrapper.update()
-      wrapper.instance().undo()
-      wrapper.update()
+      wrapper.update();
+      wrapper.instance().undo();
+      wrapper.update();
       expect(wrapper.instance().state.currentPlayer).toEqual('X');
       expect(wrapper.instance().state.message).toEqual("Player 1's Turn");
     });
@@ -197,28 +197,68 @@ describe('<App>', () => {
     it("should not alet when there's nothing to undo", () => {
       const wrapper = shallow(<App />);
       jest.spyOn(window, 'alert').mockImplementation(() => { });
-      wrapper.instance().undo(1)
-      wrapper.update()
+      wrapper.instance().undo(1);
+      wrapper.update();
       expect(window.alert).toBeCalled();
     });
   })
 
-  describe('set winner function', () => {
+  describe('setWinner function', () => {
 
     it('sets Player 1 as winner if X wins', () => {
       const wrapper = mount(<App />);
-      wrapper.instance().setWinner('X')
-      wrapper.update()
+      wrapper.instance().setWinner('X');
+      wrapper.update();
       expect(wrapper.instance().state.winner).toEqual('Player1 (X)');
       expect(wrapper.instance().state.message).toEqual('Player1 (X) Wins!');
     });
 
     it('sets Player 2 as winner if O wins', () => {
       const wrapper = mount(<App />);
-      wrapper.instance().setWinner('X')
-      wrapper.update()
+      wrapper.instance().setWinner('X');
+      wrapper.update();
       expect(wrapper.instance().state.winner).toEqual('Player1 (X)');
       expect(wrapper.instance().state.message).toEqual('Player1 (X) Wins!');
     });
   });
+
+  describe('checkForWinner function', () => {
+    const rowWinner = ['X', 'X', 'X', 'O', 'O', '', '', '', '',];
+    const columnWinner = ['', 'X', 'O', '', 'X', 'O', '', 'X', '',];
+    const diagonalWinner = ['', 'O', 'X', 'O', 'X', '', 'X', '', '',];
+    const notWinner = ['X', 'X', 'O', 'O', 'O', 'X', 'X', 'O', 'X',];
+
+    it('should find a winner in rows', () => {
+      const wrapper = mount(<App />);
+      wrapper.instance().setState({ grid: rowWinner });
+      wrapper.instance().checkForWinner()
+      wrapper.update();
+      expect(wrapper.instance().state.winner).toEqual('Player1 (X)');
+    });
+
+    it('should find a winner in columns', () => {
+      const wrapper = mount(<App />);
+      wrapper.instance().setState({ grid: columnWinner });
+      wrapper.instance().checkForWinner()
+      wrapper.update();
+      expect(wrapper.instance().state.winner).toEqual('Player1 (X)');
+    });
+
+    it('should find a winner in diagonals', () => {
+      const wrapper = mount(<App />);
+      wrapper.instance().setState({ grid: diagonalWinner });
+      wrapper.instance().checkForWinner()
+      wrapper.update();
+      expect(wrapper.instance().state.winner).toEqual('Player1 (X)');
+    });
+
+    it('should not find a winner in wrong positions', () => {
+      const wrapper = mount(<App />);
+      wrapper.instance().setState({ grid: notWinner });
+      wrapper.instance().checkForWinner()
+      wrapper.update();
+      expect(wrapper.instance().state.winner).toEqual('');
+    });
+  });
+
 });
