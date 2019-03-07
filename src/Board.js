6ecd32ebe15c21d1c7c1ec1c7958aed18db6d63b
aknishi <adriankawanishi@gmail.com>
2019-03-06 16:26:13 -0800

    Passed test

--- src/Board.js
+++ src/Board.js
@@ -3,9 +3,15 @@ import Square from './Square';
 
 export default class Board extends Component {
   render() {
+    const children = []
+    for (let i = 0; i < 9; i++) {
+      children.push(
+        <Square key={i} />
+      )
+    }
     return (
       <div className="board">
-        <Square />
+        {children}
       </div>
     )
   }
