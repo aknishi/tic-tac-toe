3bef61fa403380b065da365fd6b56aa1295b8fe5
aknishi <adriankawanishi@gmail.com>
2019-03-06 17:10:00 -0800

    Empty grid test passed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import Board from './Board';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      grid: ['', '', '', '', '', '', '', '', ''],
+    }
+  }
   render() {
     return (
       <div className="App">
