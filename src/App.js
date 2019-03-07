1ba5799dd0c7e439bba6fe4cd219ce6c0603ea9a
aknishi <adriankawanishi@gmail.com>
2019-03-06 16:06:04 -0800

    Passed Board render Test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Board from './Board';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>TIC-TAC-TOE</h1>
+        <Board />
       </div>
     );
   }
---
+++ src/Board.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Board extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is where the Board will show</h2>
+      </div>
+    )
+  }
+}
