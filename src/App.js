1a88bf51a5a9c342dc8eaf086ae805e67245fa1c
aknishi <adriankawanishi@gmail.com>
2019-03-06 16:03:20 -0800

    Test render Board and organize tests in test folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-
+      <div className="App">
+        <h1>TIC-TAC-TOE</h1>
+      </div>
     );
   }
 }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from 'react-testing-library';
-import App from './App'
+import { shallow } from 'enzyme';
+import App from '../App';
 
 describe('<App>', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should render Board', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Board').exists()).toBeTruthy();
   });
 });
